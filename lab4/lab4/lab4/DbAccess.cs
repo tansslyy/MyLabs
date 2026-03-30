8d6e9a6d617b71af1151adb10cb8d4bbec9b6c08
tansslyy <2501tsaul@gmail.com>
2026-03-30 16:43:19 +0300

    task3

---
+++ lab4/lab4/lab4/DbAccess.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows;
+
+namespace lab4 // Назва вашого проєкту
+{
+    public class DbAccess
+    {
+        private DataTable clientsTable = null;
+
+        public DataTable GetClientsTable()
+        {
+            if (clientsTable != null) return clientsTable;
+
+            clientsTable = new DataTable();
+
+            // БЕЗПЕЧНЕ ЗЧИТУВАННЯ:
+            var configSettings = ConfigurationManager.ConnectionStrings["MyDbConnection"];
+
+            // Якщо налаштування не знайдено, показуємо віконце і зупиняємо підключення
+            if (configSettings == null)
+            {
+                MessageBox.Show("Помилка: Не знайдено 'MyDbConnection' у файлі App.config! Перевірте правильність написання назви.");
+                return clientsTable;
+            }
+
+            string connectionString = configSettings.ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = connection.CreateCommand();
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                command.CommandText = "SELECT Id, Name, Phone, Address, OrderAmount FROM Clients";
+
+                try
+                {
+                    adapter.Fill(clientsTable);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Помилка підключення до БД: " + ex.Message);
+                }
+            }
+
+            return clientsTable;
+        }
+    }
+}
---
+++ lab4/lab4/lab4/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace lab4 // Назва вашого проєкту
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Метод для отримання контексту даних (Викликається при завантаженні вікна)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Викликаємо клас, який ми створили в Завданні 2
+            DbAccess db = new DbAccess();
+
+            // Передаємо таблицю як контекст даних для нашого списку
+            list.DataContext = db.GetClientsTable();
+
+            // Якщо в базі є дані, автоматично виділяємо першого клієнта
+            if (list.Items.Count > 0)
+            {
+                list.SelectedIndex = 0;
+            }
+            list.Focus();
+        }
+    }
+}
